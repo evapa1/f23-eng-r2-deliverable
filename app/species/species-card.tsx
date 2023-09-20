df8c1e07f55292a498855f2c7325e5bff342c974
evapa1 <evapierreantoine@gmail.com>
2023-09-20 01:26:03 -0400

    r

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -17,7 +17,14 @@ import { z } from "zod";
 
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
-export default function SpeciesCard(species: Species, userId: string) {
+interface PropType {
+  species: Species;
+  profile: string;
+}
+
+export default function SpeciesCard({ species, profile }: PropType) {
+  // <SpeciesCard species = Species profile = " " />
+
   const [open, setOpen] = useState<boolean>(false);
   const [edit, setEdit] = useState<boolean>(false);
 
@@ -66,6 +73,13 @@ export default function SpeciesCard(species: Species, userId: string) {
     kingdom: "Animalia",
   };
 
+  const mystyle = {
+    color: "black",
+    backgroundColor: "silver",
+    padding: "10px",
+    fontFamily: "Arial",
+  };
+
   const onSubmit = async (input: FormData) => {
     // The `input` prop contains data that has already been processed by zod. We can now use it in a supabase query
     const supabase = createClientComponentClient<Database>();
@@ -73,7 +87,6 @@ export default function SpeciesCard(species: Species, userId: string) {
       .from("species")
       .update([
         {
-          author: userId,
           common_name: input.common_name,
           description: input.description,
           kingdom: input.kingdom,
@@ -82,7 +95,7 @@ export default function SpeciesCard(species: Species, userId: string) {
           image: input.image,
         },
       ])
-      .eq("userId", userId);
+      .eq("species", species.id);
 
     if (error) {
       return toast({
@@ -99,167 +112,169 @@ export default function SpeciesCard(species: Species, userId: string) {
   };
 
   return (
-    <div className="min-w-72 m-4 w-72 flex-none rounded border-2 p-3 shadow">
-      {species.image && (
-        <div className="relative h-40 w-full">
-          <Image src={species.image} alt={species.scientific_name} fill style={{ objectFit: "cover" }} />
-        </div>
-      )}
-      <h3 className="mt-3 text-2xl font-semibold">{species.common_name}</h3>
-      <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
-      <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
-      {/* Replace with detailed view */}
-      <Button className="mt-3 w-full" onClick={() => setOpen(true)}>
-        Learn More
-      </Button>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
-          <DialogHeader>
-            <DialogTitle>Species</DialogTitle>
-            {/* make it look nicer */}
-            <DialogDescription>Scientific name: {species.scientific_name}</DialogDescription>
-            <DialogDescription>Common name: {species.common_name}</DialogDescription>
-            <DialogDescription>Total Population: {species.total_population}</DialogDescription>
-            <DialogDescription>Kingdom: {species.kingdom}</DialogDescription>
-            <DialogDescription>Description: {species.description}</DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-      <Button variant="secondary" onClick={() => setEdit(true)}>
-        Edit
-      </Button>
-      <Dialog open={edit} onOpenChange={setEdit}>
-        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
-          <Form {...form}>
-            <form onSubmit={(e: BaseSyntheticEvent) => void form.handleSubmit(onSubmit)(e)}>
-              <div className="grid w-full items-center gap-4">
-                <FormField
-                  control={form.control}
-                  name="scientific_name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Scientific Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder={species.scientific_name} {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="common_name"
-                  render={({ field }) => {
-                    // We must extract value from field and convert a potential defaultValue of `null` to "" because inputs can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
-                    const { value, ...rest } = field;
-                    return (
+    <div>
+      <div className="min-w-72 m-4 w-72 flex-none rounded border-2 p-3 shadow">
+        {species.image && (
+          <div className="relative h-40 w-full">
+            <Image src={species.image} alt={species.scientific_name} fill style={{ objectFit: "cover" }} />
+          </div>
+        )}
+        <h3 className="mt-3 text-2xl font-semibold">{species.common_name}</h3>
+        <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
+        <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
+        {/* Replace with detailed view */}
+        <Button className="mt-3 w-full" onClick={() => setOpen(true)}>
+          Learn More
+        </Button>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+            <DialogHeader>
+              {/* make it look nicer */}
+              <DialogDescription style={mystyle}>Scientific name: {species.scientific_name}</DialogDescription>
+              <DialogDescription style={mystyle}>Common name: {species.common_name}</DialogDescription>
+              <DialogDescription style={mystyle}>Total Population: {species.total_population}</DialogDescription>
+              <DialogDescription style={mystyle}>Kingdom: {species.kingdom}</DialogDescription>
+              <DialogDescription style={mystyle}>Image URL: {species.image}</DialogDescription>
+              <DialogDescription style={mystyle}>Description: {species.description}</DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+        <Button variant="secondary" onClick={() => setEdit(true)}>
+          Edit
+        </Button>
+        <Dialog open={edit} onOpenChange={setEdit}>
+          <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+            <Form {...form}>
+              <form onSubmit={(e: BaseSyntheticEvent) => void form.handleSubmit(onSubmit)(e)}>
+                <div className="grid w-full items-center gap-4">
+                  <FormField
+                    control={form.control}
+                    name="scientific_name"
+                    render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Common Name</FormLabel>
+                        <FormLabel>Scientific Name</FormLabel>
                         <FormControl>
-                          <Input value={value ?? ""} placeholder={species.common_name} {...rest} />
+                          <Input placeholder={species.scientific_name} {...field} />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
-                    );
-                  }}
-                />
-                <FormField
-                  control={form.control}
-                  name="kingdom"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Kingdom</FormLabel>
-                      {/* Using shadcn/ui form with enum: https://github.com/shadcn-ui/ui/issues/772 */}
-                      <Select
-                        onValueChange={(value) => field.onChange(kingdoms.parse(value))}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder={species.kingdom} />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectGroup>
-                            {kingdoms.options.map((kingdom, index) => (
-                              <SelectItem key={index} value={kingdom}>
-                                {kingdom}
-                              </SelectItem>
-                            ))}
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="total_population"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Total population</FormLabel>
-                      <FormControl>
-                        {/* Using shadcn/ui form with number: https://github.com/shadcn-ui/ui/issues/421 */}
-                        <Input
-                          type="number"
-                          placeholder={species.total_population}
-                          {...field}
-                          onChange={(event) => field.onChange(+event.target.value)}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="image"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Image URL</FormLabel>
-                      <FormControl>
-                        <Input placeholder={species.image} {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => {
-                    // We must extract value from field and convert a potential defaultValue of `null` to "" because textareas can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
-                    const { value, ...rest } = field;
-                    return (
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="common_name"
+                    render={({ field }) => {
+                      // We must extract value from field and convert a potential defaultValue of `null` to "" because inputs can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
+                      const { value, ...rest } = field;
+                      return (
+                        <FormItem>
+                          <FormLabel>Common Name</FormLabel>
+                          <FormControl>
+                            <Input value={value ?? ""} placeholder={species.common_name} {...rest} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="kingdom"
+                    render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Description</FormLabel>
+                        <FormLabel>Kingdom</FormLabel>
+                        {/* Using shadcn/ui form with enum: https://github.com/shadcn-ui/ui/issues/772 */}
+                        <Select
+                          onValueChange={(value) => field.onChange(kingdoms.parse(value))}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder={species.kingdom} />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectGroup>
+                              {kingdoms.options.map((kingdom, index) => (
+                                <SelectItem key={index} value={kingdom}>
+                                  {kingdom}
+                                </SelectItem>
+                              ))}
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="total_population"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Total population</FormLabel>
                         <FormControl>
-                          <Textarea value={value ?? ""} placeholder={species.description} {...rest} />
+                          {/* Using shadcn/ui form with number: https://github.com/shadcn-ui/ui/issues/421 */}
+                          <Input
+                            type="number"
+                            placeholder={species.total_population}
+                            {...field}
+                            onChange={(event) => field.onChange(+event.target.value)}
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
-                    );
-                  }}
-                />
-                <div className="flex">
-                  <Button type="submit" className="ml-1 mr-1 flex-auto">
-                    Edit Species
-                  </Button>
-                  <Button
-                    type="button"
-                    className="ml-1 mr-1 flex-auto"
-                    variant="secondary"
-                    onClick={() => setEdit(false)}
-                  >
-                    Cancel
-                  </Button>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="image"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Image URL</FormLabel>
+                        <FormControl>
+                          <Input placeholder={species.image} {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => {
+                      // We must extract value from field and convert a potential defaultValue of `null` to "" because textareas can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
+                      const { value, ...rest } = field;
+                      return (
+                        <FormItem>
+                          <FormLabel>Description</FormLabel>
+                          <FormControl>
+                            <Textarea value={value ?? ""} placeholder={species.description} {...rest} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                  <div className="flex">
+                    <Button type="submit" className="ml-1 mr-1 flex-auto">
+                      Edit Species
+                    </Button>
+                    <Button
+                      type="button"
+                      className="ml-1 mr-1 flex-auto"
+                      variant="secondary"
+                      onClick={() => setEdit(false)}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   );
 }
